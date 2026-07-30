c9ee688b5362aca060425ea3698102c2128e99f6
Rediet Sol <ritatati837@gmail.com>
2026-07-30 05:18:20 +0300

    Remove stray duplicate PagedRequest.cs from old Dtos folder

--- obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a94b734d4a95def2c3597948b350d483aeffd30d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+820cc16e0be5f00b767d06dae39c1ae8eadc9636")]
 [assembly: System.Reflection.AssemblyProductAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
